7ec02cfaf0cbd414440acc6d567605f572767a0a
Month <10637744+gcc2003@user.noreply.gitee.com>
2026-06-17 19:19:11 +0800

    修改配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: 'https://github.com/ofthenew2003/WallpaperTool',
+  base: '/WallpaperTool/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
